7a9809aee3244ad8cb23eb0e54366c56361bcdfd
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-06-14 15:54:18 +0700

    Create server

--- app.js
+++
@@ -1,8 +0,0 @@
-const os = require("os");
-console.log(os.platform(), os.homedir());
-
-const { names, ages } = require("./people");
-console.log(names, ages);
-
-console.log(__dirname);
-console.log(__filename);
--- people.js
+++
@@ -1,4 +0,0 @@
-const names = ["John", "Jane", "Michael", "Emily", "David", "Sarah"];
-const ages = [25, 30, 42, 18, 55, 37];
-
-module.exports = { names, ages };
