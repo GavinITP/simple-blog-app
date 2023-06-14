adeea135528f634ce572b312eb6b055deef74e34
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-06-14 16:29:00 +0700

    create respond

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("request comes in");
-}); // callback function that run every time request come in
+  console.log(req.url, req.method);
 
-//localhost:3000
-server.listen(3000, "localhost", () => {
-  // loopback ip , port = door into server
-  console.log("listening for request on port 3000");
+  // set header content type
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Hello I'm server");
+  res.end();
 });
+
+server.listen(3000, "localhost", () => {});
