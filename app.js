1bcb40c46cb9e9b9019531d0f628d5dccf52ca2e
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-06-15 14:53:21 +0700

    Redirect and 404

--- app.js
+++ app.js
@@ -11,3 +11,15 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root: __dirname });
 });
+
+// redirect
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// do every time req comes in, but when it reaches this
+// like default in switch case
+app.use((req, res) => {
+  // res.status() returns res
+  res.status(400).sendFile("./views/404.html", { root: __dirname });
+});
