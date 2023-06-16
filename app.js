d2d0ba0f36a7a19b2e4e0d9259d02d90b1b966f3
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-06-16 14:37:02 +0700

    Middleware

--- app.js
+++ app.js
@@ -2,24 +2,54 @@ const express = require("express");
 
 const app = express();
 
+// view engine
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
+app.use((req, res, next) => {
+  console.log("new request made");
+  console.log("host:", req.hostname);
+  console.log("path:", req.path);
+  console.log("method:", req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Hi I'm second middleware");
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+
+  res.render("index", { title: "Home", blogs: blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirect
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create new blog" });
 });
 
 // do every time req comes in, but when it reaches this
 // like default in switch case
 app.use((req, res) => {
   // res.status() returns res
-  res.status(400).sendFile("./views/404.html", { root: __dirname });
+  res.status(400).render("404", { title: "404" });
 });
